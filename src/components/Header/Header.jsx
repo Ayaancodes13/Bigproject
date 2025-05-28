cd80d4aa26d639ceb33f49fb2b3b19a6a5c7d3ae
Ayaancodes13 <ayaan13.aak@gmail.com>
2025-05-28 23:52:25 +0500

    Updating Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import LogOutBtn from './LogOutBtn'
 const Header = () => {
+  const selector = useSelector()
   return (
-    <div>
-      
-    </div>
+    <header>
+
+    </header>
   )
 }
 export default Header 
